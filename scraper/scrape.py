36125a5aa4d89fda3d36d67a27de0431d1d6f476
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2020-10-22 02:44:29 +0700

    add: python web scraping script

---
+++ scraper/scrape.py
@@ -0,0 +1,26 @@
+
+# web scraping script to get all the dino names from the www.nhm.ac.uk
+
+from bs4 import BeautifulSoup
+import requests
+
+# obtain the html file
+html = requests.get(
+    'https://www.nhm.ac.uk/discover/dino-directory/name/name-az-all.html').text
+
+# parse the html file
+parsed_html = BeautifulSoup(html)
+
+# isolate your tags to a list
+dinos = parsed_html.find_all(
+    'li', 'dinosaurfilter--dinosaur dinosaurfilter--all-list')
+
+# isolate further
+dino_names = []
+for dinosaur in dinos:
+    name = dinosaur.find(
+        'p', 'dinosaurfilter--name dinosaurfilter--name-unhyphenated').get_text()
+    dino_names.append(name.strip())
+
+# profit
+print(dino_names)
